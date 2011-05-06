5af6de440dc2214bf9dabbf52156c2f46adb40d9
martin <martin@hop.kcl.ac.uk>
2011-05-06 23:42:26 +0100

    deflate still doesn't work

--- png.lisp
+++ png.lisp
@@ -1,124 +1,4 @@
 
-;; http://en.wikipedia.org/wiki/PNG_file_format
-;; http://garethrees.org/2007/11/14/pngcrush/
-;; http://www.ietf.org/rfc/rfc1951.txt
-;; http://drj11.wordpress.com/2007/11/20/a-use-for-uncompressed-pngs/
-;; http://www.w3.org/TR/PNG/#11IHDR
-
-;; for letters start chunk
-;; 1) uppercase letter = critical
-;; 2) uppercase = public
-;; 3) must be uppercase
-;; 4) lowercase = safe to copy
-
-;; chunk:
-;; length ub4, chunk-type ub4, chunk-data length*ub, crc ub4
-
-;; IHDR 73 72 68 82 first chunk with header
-
-;; width ub4, height ub4, bit-depth=8, color-type=0,
-;; compression-method=0, filter-method=0, interlace-method=0
- 
-;; IDAT 73 68 65 84 image
-
-;; ZLIB
-;; compression-method-and-flags=#b0111 1000=#x78, flags= #x9c , 
-;; CMF*256+FLG must be multiple of 31
-#+nil ;; FLG & #b11111 =
-(mod (* 257 #x78) 31)
-#+nil
-(logand #x9c #b11111)
-
-;; FCHECK=11100 no preset dictionary, default algorithm
-
-;; inside #0500 for n=5
-;; followed by two's complement #xfaff
-
-;; datablocks n*1byte, check-value ub4 (adler)
-
-;; DEFLATE
-;; header-bits (3) BFINAL1, BTYPE2=00
-
-;; ignores all data until next byte boundary, then
-;; LEN2,NLEN2, LEN*byte data
-
-;; IEND 73 69 78 68 marks end of image
-
-(defun list->array (ls)
-  (declare (values (simple-array (unsigned-byte 8) 1) &optional))
-  (make-array (length ls) :element-type '(unsigned-byte 8)
-	      :initial-contents ls))
-
-(defun ub32->ub8 (b)
-  (loop for i below 4 collect
-       (ldb (byte 8 (* 8 i)) b)))
-(defun ub16->ub8 (b)
-  (loop for i below 2 collect
-       (ldb (byte 8 (* 8 i)) b)))
-
-#+nil
-(ub4->ub1 #xff010203)
-
-(defun ihdr (width height &optional (bit-depth 8) (color-type 0)
-	     (compression-method 0) (filter-method 0) (interlace-method 0))
-  (declare (type (unsigned-byte 32) width height)
-	   (type (unsigned-byte 8) bit-depth color-type compression-method
-		 filter-method interlace-method)
-	   (values (simple-array (unsigned-byte 8) 1) &optional))
-  (let* ((data (list width height bit-depth color-type compression-method filter-method
-		     interlace-method)))
-    (list->array (append (ub16->ub8 (length data))
-			 (list 73 72 68 82)
-			 data 
-			 (ub32->ub8 (crc (list->array data)))))))
-
-(defun idat (zlib-data)
-  (declare (type (simple-array (unsigned-byte 8) 1) zlib-data)
-	   (values (simple-array (unsigned-byte 8) 1) &optional))
-  (concatenate '(simple-array (unsigned-byte 8) 1)
-	       (list->array (append (ub16->ub8 (length zlib-data))
-				    (list 73 68 65 84)))
-	       zlib-data
-	       (list->array (ub32->ub8 (crc zlib-data)))))
-
-(defun zlib (deflate-data)
-  (declare (type (simple-array (unsigned-byte 8) 1) deflate-data)
-	   (values (simple-array (unsigned-byte 8) 1) &optional))
-  (concatenate '(simple-array (unsigned-byte 8) 1)
-	       (list->array (list #x78 #x9c))
-	       deflate-data
-	       (list->array (ub32->ub8 (adler deflate-data)))))
-
-(defun deflate (image-data)
-  (concatenate '(simple-array (unsigned-byte 8) 1)
-	       (list->array (append (list #b10000000)
-				    (ub16->ub8 (length image-data))
-				    (ub16->ub8 (- (length image-data)))))
-	       image-data))
-
-(defun iend ()
-  (list->array (list 73 69 78 68)))
-
-(defun write-png (fn image-data)
-  (declare (type (simple-array (unsigned-byte 8) 2) image-data))
-  (with-open-file (s fn :direction :output
-		     :if-exists :supersede
-		     :if-does-not-exist :create
-		     :element-type '(unsigned-byte 8))
-   (destructuring-bind (h w) (array-dimensions image-data)
-     (write-sequence 
-      (concatenate '(simple-array (unsigned-byte 8) 1)
-		   (list->array '(137 80 78 71 13 10 26 10)) ;; signature
-		   (ihdr w h)
-		   (idat (zlib (deflate (sb-ext:array-storage-vector image-data))))
-		   (iend))
-      s))
-   nil))
-
-(let* ((h 100)
-       (w 100)
-       (data (make-array (list h w) :element-type '(unsigned-byte 8))))
-  (write-png "/dev/shm/o.png" data))
 
 (let ((crc-table 
        (make-array 
@@ -160,7 +40,6 @@
 	      (coerce "Twas brillig and the slithy toves did gyre and gimble in the wabe" 
 		      'list)))) ; 4186783197
 
-
 (defun adler (buf)
   (declare (type (simple-array (unsigned-byte 8) 1) buf)
 	   (values (unsigned-byte 32) &optional))
@@ -183,3 +62,147 @@
 	(mapcar #'(lambda (x) (char-code x)) 
 		(coerce "blah" 
 			'list)))) ;; #x03fa0198
+
+
+
+;; http://en.wikipedia.org/wiki/PNG_file_format
+;; http://garethrees.org/2007/11/14/pngcrush/
+;; http://www.ietf.org/rfc/rfc1951.txt
+;; http://drj11.wordpress.com/2007/11/20/a-use-for-uncompressed-pngs/
+;; http://www.w3.org/TR/PNG/#11IHDR
+
+;; for letters start chunk
+;; 1) uppercase letter = critical
+;; 2) uppercase = public
+;; 3) must be uppercase
+;; 4) lowercase = safe to copy
+
+;; chunk:
+;; length ub4, chunk-type ub4, chunk-data length*ub, crc ub4
+
+;; all numbers in network byte order (most significant byte first)
+;; crc includes chunk-type and chunk-data (but not length)
+
+;; IHDR 73 72 68 82 first chunk with header
+
+;; width ub4, height ub4, bit-depth=8, color-type=0,
+;; compression-method=0, filter-method=0, interlace-method=0
+ 
+;; IDAT 73 68 65 84 image
+
+;; ZLIB
+;; compression-method-and-flags=#b0111 1000=#x78, flags= #x9c ,
+;; CM=8 bits 0 to 3
+;; CINFO=base-2 log of lz77 window size - 8 
+;; CMF*256+FLG must be multiple of 31
+#+nil ;; FLG & #b11111 =
+(mod (* 257 #x78) 31)
+#+nil
+(logand #x9c #b11111)
+
+;; FCHECK=11100 no preset dictionary, default algorithm
+
+;; inside #0500 for n=5
+;; followed by two's complement #xfaff
+
+;; datablocks n*1byte, check-value ub4 (adler)
+
+;; DEFLATE
+;; header-bits (3) BFINAL1, BTYPE2=00
+
+;; ignores all data until next byte boundary, then
+;; LEN2,NLEN2, LEN*byte data
+
+;; IEND 73 69 78 68 marks end of image
+;; test file http://www.libpng.org/pub/png/PngSuite/basn0g08.png
+;; http://www.chrfr.de/software/midp_png.html
+(defun list->array (ls)
+  (declare (values (simple-array (unsigned-byte 8) 1) &optional))
+  (make-array (length ls) :element-type '(unsigned-byte 8)
+	      :initial-contents ls))
+
+(defun ub32->ub8 (b)
+  (reverse
+   (loop for i below 4 collect
+	(ldb (byte 8 (* 8 i)) b))))
+(defun ub16->ub8 (b)
+  (reverse
+   (loop for i below 2 collect
+	(ldb (byte 8 (* 8 i)) b))))
+
+#+nil
+(ub4->ub1 #xff010203)
+
+(defun ihdr (width height &optional (bit-depth 8) (color-type 0)
+	     (compression-method 0) (filter-method 0) (interlace-method 0))
+  (declare (type (unsigned-byte 32) width height)
+	   (type (unsigned-byte 8) bit-depth color-type compression-method
+		 filter-method interlace-method)
+	   (values (simple-array (unsigned-byte 8) 1) &optional))
+  (let* ((data (append (ub32->ub8 width)
+		       (ub32->ub8 height)
+		       (list bit-depth color-type compression-method 
+				      filter-method interlace-method)))
+	 (signature (list 73 72 68 82)))
+    (list->array (append (ub32->ub8 (length data))
+			 signature
+			 data
+			 (ub32->ub8 (crc (list->array (append signature
+							      data))))))))
+
+(defun idat (zlib-data)
+  (declare (type (simple-array (unsigned-byte 8) 1) zlib-data)
+	   (values (simple-array (unsigned-byte 8) 1) &optional))
+  (let ((signature (list 73 68 65 84)))
+   (concatenate '(simple-array (unsigned-byte 8) 1)
+		(list->array (append (ub32->ub8 (length zlib-data))
+				     signature))
+		zlib-data
+		(list->array (ub32->ub8 (crc (concatenate '(simple-array (unsigned-byte 8) 1) 
+							  (list->array signature)
+							  zlib-data)))))))
+
+(defun zlib (deflate-data)
+  (declare (type (simple-array (unsigned-byte 8) 1) deflate-data)
+	   (values (simple-array (unsigned-byte 8) 1) &optional))
+  (concatenate '(simple-array (unsigned-byte 8) 1)
+	       (let ((cmf 8))
+		(list->array (list cmf (mod (- 31 (mod (ash cmf 8) 31)) 31))))
+	       deflate-data
+	       (list->array (ub32->ub8 (adler deflate-data)))))
+
+(defun deflate (image-data)
+  (concatenate '(simple-array (unsigned-byte 8) 1)
+	       (list->array (append (list #b10000000)
+				    (ub16->ub8 (length image-data))
+				    (ub16->ub8 (- (length image-data)))))
+	       image-data))
+
+(defun iend ()
+  (let ((signature (list 73 69 78 68)))
+    (list->array (append (ub32->ub8 0) 
+			 signature 
+			 (ub32->ub8 (crc (list->array signature)))))))
+
+(defun write-png (fn image-data)
+  (declare (type (simple-array (unsigned-byte 8) 2) image-data))
+  (with-open-file (s fn :direction :output
+		     :if-exists :supersede
+		     :if-does-not-exist :create
+		     :element-type '(unsigned-byte 8))
+   (destructuring-bind (h w) (array-dimensions image-data)
+     (write-sequence 
+      (concatenate '(simple-array (unsigned-byte 8) 1)
+		   (list->array '(137 80 78 71 13 10 26 10)) ;; signature
+		   (ihdr w h)
+		   (idat (zlib (deflate 
+				   (sb-ext:array-storage-vector image-data))))
+		   (iend))
+      s))
+   nil))
+
+
+(let* ((h 32)
+       (w 32)
+       (data (make-array (list h w) :element-type '(unsigned-byte 8))))
+  (write-png "/dev/shm/o.png" data))
